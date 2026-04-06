5d89381889a67c00c4e379e715cd7ca43025b5d9
MROCTOPUS456 <167027091+MROCTOPUS456@users.noreply.github.com>
2026-04-06 10:28:21 +0700

    Update kaitun sailor.lua

--- kaitun sailor.lua
+++ kaitun sailor.lua
@@ -1,46 +1,14 @@
 script_key="LwdPGikHCzYWCluCgqYpQpKarqyItkRF";
-getgenv().SailorPieceConfig = getgenv().SailorPieceConfig or {
-    OpenChests       = {
-        "Common Chest",
-        "Rare Chest",
-        "Epic Chest",
-        "Legendary Chest",
+getgenv().SailorVIP = {
+    Optimization = {
+        Enable = false, -- On/Off toàn bộ optimization script
+        FPSCap = 10, -- Số FPS cần lock (ví dụ: 10, 15, 30, 60)
     },
-    AutoAscend = true,
-    AutoUpgradeAscend = true,
-    Debug            = false,
-    Performance = {
-        AutoRejoinIfHighRam = 5000,
-        WebhookURL      = "https://discord.com/api/webhooks/1489640128463769641/IWV0LxxCuJCDe0e1BSneVKaf-70GniP2BsPEMzbdiU6dWEf8Nrkyeop0ph7Y6TxL62AE",
-        FPSBoost        = false,
-        FPSLock         = 20,
-    },
-    SkillTree = {
-        Auto            = true,
-        Priority        = {"Luck", "Damage", "CritChance", "CritDamage", "HP"},
-    },
-    AutoStrongestMelee = true,
-    Reroll = {
-        Trait = {
-            Auto            = true,
-            TargetRarity    = "Secret",
-        },
-        TargetRace      = {"Kitsune"},
-        TargetClan      = {"Eminence", "Monarch"},
-        },
-    Artifact = {
-        AutoEquip       = true,
-        AutoUpgrade     = true,
-        AutoDeleteTrash = true,
-        UpgradeSet      = {"Celestial Rupture"},-- Ashen Chronicles, Crownless Regalia, Black Horizon, Void Reaver, Celestial Rupture, Abyssal Crown
-        UpgradeStopAt   = 15,
-        TrashRarities   = {"Common", "Rare", "Epic"},
+
+    Webhook = {
+        Enable = true, -- On/Off gửi webhook thông báo crate
+        URL = "https://discord.com/api/webhooks/1489640128463769641/IWV0LxxCuJCDe0e1BSneVKaf-70GniP2BsPEMzbdiU6dWEf8Nrkyeop0ph7Y6TxL62AE", 
     },
 }
-while true do
-    local success = pcall(function()
-        loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/1169527463a6fb002d07c345110bc0aa.lua"))()
-    end)
-    if success then break end
-    task.wait(5)
-end
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/a8255d759144bb6127c5b72221cce5cd.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/list/refs/heads/main/game"))()
